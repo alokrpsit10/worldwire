326866173fe8385484d56bbf159008b22f7d2992
alokrpsit10 <alok.rpsit10@gmail.com>
2026-06-17 14:06:08 +0530

    news.js

---
+++ netlify/functions/news.js
@@ -0,0 +1,39 @@
+const https = require('https');
+
+exports.handler = async function(event) {
+  const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+  const params = event.queryStringParameters || {};
+  const search = params.search || '';
+  const cat = params.category || 'general';
+  const country = params.country || 'us';
+
+  let url = '';
+  if (search) {
+    url = 'https://newsapi.org/v2/everything?q=' + encodeURIComponent(search) + '&language=en&sortBy=publishedAt&pageSize=20&apiKey=' + API_KEY;
+  } else {
+    url = 'https://newsapi.org/v2/top-headlines?country=' + country + '&category=' + cat + '&pageSize=20&apiKey=' + API_KEY;
+  }
+
+  return new Promise(function(resolve) {
+    https.get(url, function(res) {
+      var data = '';
+      res.on('data', function(chunk) { data += chunk; });
+      res.on('end', function() {
+        resolve({
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: data,
+        });
+      });
+    }).on('error', function(err) {
+      resolve({
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({ status: 'error', message: err.message }),
+      });
+    });
+  });
+};
